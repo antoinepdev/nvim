bf86dd683a28194b5e9dee8783e07f3ea68bd40c
* <antoinepenadev@gmail.com>
2025-12-08 17:50:52 -0500

    feat: config basic editor options

---
+++ lua/dev/core/opts.lua
@@ -0,0 +1,28 @@
+local opt = vim.opt
+
+-- Ui
+opt.number = true -- show line number
+opt.relativenumber = true -- relative line number
+opt.wrap = false
+opt.cursorline = true -- highlight the current cursor line
+opt.termguicolors = true
+opt.background = "dark" -- colorschemes that can be light or dark will be made dark
+opt.signcolumn = "yes" -- show sign column so that text doesn't shift
+opt.scrolloff = 10 -- Minimal number of screen lines to keep above and below the cursor.
+
+
+vim.o.confirm = true -- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`), instead raise a dialog asking if you wish to save the current file(s) See `:help 'confirm'`
+
+
+-- Tabs & Indentation
+opt.tabstop = 2 -- number of spaces for tabs
+opt.shiftwidth = 2 -- number of spaces for each step of (auto)indent
+opt.expandtab = true -- expand tab to spaces
+opt.autoindent = true -- copy indent from current line when starting new one
+opt.list = true -- Show <tab> and trailing spaces
+
+
+-- Search Settings
+opt.ignorecase = true -- ignore case when searching
+opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive (ignorecase should be true to work)
+-- opt.wrapscan = false -- when the search reaches the end of the file it will not continue at the start
