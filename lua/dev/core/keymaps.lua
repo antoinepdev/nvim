b77700103eddcbdbfd49ecce516e8bdfa8915b0d
* <antoinepenadev@gmail.com>
2025-12-08 20:57:34 -0500

    feat(keymaps): set keymaps for nvim terminal

--- lua/dev/core/keymaps.lua
+++ lua/dev/core/keymaps.lua
@@ -32,3 +32,9 @@ map("n", "gh", "<C-w>h", {desc="Move to left window"})
 map("n", "gl", "<C-w>l", {unique = false, desc="Move to left window"})
 map("n", "gj", "<C-w>j", {desc="Move to bottom window"})
 map("n", "gk", "<C-w>k", {desc="Move to top window"})
+
+
+-- Terminal
+map("n", "<leader>tt", "<cmd>tabnew<CR><cmd>term<CR>i", {desc = "Terminal: New Tab"})
+map("t", "<Esc>", "<C-\\><C-n>") -- Exit terminal mode
+map("t", "jk", "<C-\\><C-n>") -- Exit terminal mode with jk
