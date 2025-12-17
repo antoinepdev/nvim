6d7ec900282231ebaebb52816e61a1b012d17927
* <antoinepenadev@gmail.com>
2025-12-16 20:36:42 -0500

    style: format code

--- lua/dev/plugins/which-key.lua
+++ lua/dev/plugins/which-key.lua
@@ -1,26 +1,26 @@
 return {
-  'folke/which-key.nvim',
-  event = 'VeryLazy',
+  "folke/which-key.nvim",
+  event = "VeryLazy",
   opts = {
-    preset = 'helix',
+    preset = "helix",
     delay = 500,
   },
   keys = {
     {
-      '<leader>?',
-      function() require('which-key').show({ global = false }) end,
-      desc = 'Buffer Local Keymaps (which-key)',
+      "<leader>?",
+      function() require("which-key").show({ global = false }) end,
+      desc = "Buffer Local Keymaps (which-key)",
     },
   },
 
   config = function(_, opts)
-    local wk = require('which-key')
+    local wk = require("which-key")
 
     wk.add({
       -- Basics
-      { '<leader>t', group = 'Terminal' },
-      { '<leader>d', group = 'Diagnostics' },
-      { '<leader>?', '<Ignore>', desc = 'which_key_ignore' },
+      { "<leader>t", group = "Terminal" },
+      { "<leader>d", group = "Diagnostics" },
+      { "<leader>?", "<Ignore>", desc = "which_key_ignore" },
     })
     wk.setup(opts)
   end,
