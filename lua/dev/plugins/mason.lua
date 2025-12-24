15c0793e2011ea2f9bd1baffdfd65b0a0b618caf
* <antoinepenadev@gmail.com>
2025-12-24 13:15:45 -0500

    perf(mason): change mason-lspconfig for mason-tool-installer

--- lua/dev/plugins/mason.lua
+++ lua/dev/plugins/mason.lua
@@ -1,7 +1,6 @@
 return {
   "mason-org/mason.nvim",
   dependencies = {
-    "mason-org/mason-lspconfig.nvim",
     "WhoIsSethDaniel/mason-tool-installer.nvim",
   },
   opts = {
@@ -15,17 +14,13 @@ return {
   },
   config = function()
     require("mason").setup()
-    local mason_lspconfig = require("mason-lspconfig")
-
-    mason_lspconfig.setup({
-      ensure_installed = {
-        "ts_ls",
-        "jsonls",
-      },
-    })
 
     require("mason-tool-installer").setup({
-      ensure_installed = {},
+      ensure_installed = {
+        "typescript-language-server",
+        "json-lsp",
+      },
+      -- auto_update = true,
     })
   end,
 }
