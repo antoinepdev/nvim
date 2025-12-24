70404cb75accb85308300680abe1f0720638cc54
* <antoinepenadev@gmail.com>
2025-12-24 11:37:23 -0500

    style: format code

--- lua/dev/core/keymaps.lua
+++ lua/dev/core/keymaps.lua
@@ -2,22 +2,21 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 local function map(mode, key, cmd, opts)
-    local default_opts = {
-        silent = true,
-        unique = true,
-        nowait = true,
-        desc = "which_key_ignore"
-    }
+  local default_opts = {
+    silent = true,
+    unique = true,
+    nowait = true,
+    desc = "which_key_ignore",
+  }
 
-    if opts == nil then
-        opts = default_opts
-    else
-        opts = vim.tbl_deep_extend("force", default_opts, opts)
-    end
+  if opts == nil then
+    opts = default_opts
+  else
+    opts = vim.tbl_deep_extend("force", default_opts, opts)
+  end
 
-    vim.keymap.set(mode, key, cmd, opts)
+  vim.keymap.set(mode, key, cmd, opts)
 end
-
 
 -- Basic keymaps
 map("n", "<leader>ww", ":w<CR>")
@@ -26,38 +25,42 @@ map("n", "<leader>Q", ":q!<CR>")
 map("i", "jk", "<ESC>") -- Exit insert mode with jk
 map("n", "<ESC>", ":nohl<CR>") -- Disable highlight with ESC
 
-
 -- Window movement
-map("n", "gh", "<C-w>h", {desc="Move to left window"})
-map("n", "gl", "<C-w>l", {unique = false, desc="Move to left window"})
-map("n", "gj", "<C-w>j", {desc="Move to bottom window"})
-map("n", "gk", "<C-w>k", {desc="Move to top window"})
-
+map("n", "gh", "<C-w>h", { desc = "Move to left window" })
+map("n", "gl", "<C-w>l", { unique = false, desc = "Move to left window" })
+map("n", "gj", "<C-w>j", { desc = "Move to bottom window" })
+map("n", "gk", "<C-w>k", { desc = "Move to top window" })
 
 -- Terminal
-map("n", "<leader>tt", "<cmd>tabnew<CR><cmd>term<CR>i", {desc = "Terminal: New Tab"})
+map(
+  "n",
+  "<leader>tt",
+  "<cmd>tabnew<CR><cmd>term<CR>i",
+  { desc = "Terminal: New Tab" }
+)
 map("t", "<Esc>", "<C-\\><C-n>") -- Exit terminal mode
 map("t", "jk", "<C-\\><C-n>") -- Exit terminal mode with jk
 
-
 -- Tabs
-map("n", "<leader>1", "<cmd>tabnew<CR>", {desc = "New Tab"})
-map("n", "<leader><Tab>", "<cmd>tabnext<CR>", {desc = "Next Tab"})
-map("n", "<leader>º", "<cmd>tabprevious<CR>", {desc = "Previous Tab"})
-
+map("n", "<leader>1", "<cmd>tabnew<CR>", { desc = "New Tab" })
+map("n", "<leader><Tab>", "<cmd>tabnext<CR>", { desc = "Next Tab" })
+map("n", "<leader>º", "<cmd>tabprevious<CR>", { desc = "Previous Tab" })
 
 -- Delete
 map("n", "x", '"_x') -- Delete an charcter without copy
 map("v", "x", '"_d') -- Delete selected code without copy
 
-
 -- Go to
-map({"n", "v"}, "gm", "%", {desc="Matching current parenthesis or bracket"})
-map("i", "<C-h>", "<Esc>i", {unique = false}) -- Skip next key
-map("i", "<C-l>", "<Esc>la", {unique = false}) -- Skip to previous key
-map({"n", "v"}, "H", "0", {unique=false}) -- Go to the beginning of the line
-map({"n", "v"}, "L", "$", {unique=false}) -- Go to the end of the line
-
+map(
+  { "n", "v" },
+  "gm",
+  "%",
+  { desc = "Matching current parenthesis or bracket" }
+)
+map("i", "<C-h>", "<Esc>i", { unique = false }) -- Skip next key
+map("i", "<C-l>", "<Esc>la", { unique = false }) -- Skip to previous key
+map({ "n", "v" }, "H", "0", { unique = false }) -- Go to the beginning of the line
+map({ "n", "v" }, "L", "$", { unique = false }) -- Go to the end of the line
 
 -- Copy
 map("n", "ya", ":%y<CR>") -- Copy all
