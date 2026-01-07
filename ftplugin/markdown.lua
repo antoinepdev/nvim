727d07ddb214594ef5e73fd3345c4a36b4147626
* <antoinepenadev@gmail.com>
2026-01-07 17:44:02 -0500

    feat(keymaps): config movement on wrapped text

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -1,3 +1,10 @@
+-- opts
 local opt = vim.opt_local
 opt.wrap = true
 opt.linebreak = true
+
+-- keymaps
+vim.keymap.set("n", "j", "gj") -- move between wrapped text
+vim.keymap.set("n", "k", "gk") -- move between wrapped text
+vim.keymap.set("n", "gj", "j") -- move between wrapped text
+vim.keymap.set("n", "gk", "k") -- move between wrapped text
