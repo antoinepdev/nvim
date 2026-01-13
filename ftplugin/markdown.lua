4392bd8ce626abc4b0e144c321c954c51a6eeeb5
antoinepdev <antoinepdeveloper@gmail.com>
2026-01-13 18:11:17 -0500

    feat: hide number line for markdown files

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -4,6 +4,8 @@ opt.wrap = true
 opt.linebreak = true
 opt.conceallevel = 2 -- hide concealed text
 opt.concealcursor = "n" -- hide concealed text in normal mode
+opt.number = false -- hide line number
+opt.relativenumber = false -- hide line number
 
 -- keymaps
 vim.keymap.set("n", "j", "gj") -- move between wrapped text
