92a8c02e70fc85842c82cd033a6e1bd54b78b993
antoinepdev <antoinepdeveloper@gmail.com>
2026-01-19 19:23:17 -0500

    style(autocmds): format code

--- lua/dev/core/autocmds.lua
+++ lua/dev/core/autocmds.lua
@@ -1,15 +1,9 @@
--- synchronize nvim clipboard with system clipboard
-vim.api.nvim_create_autocmd('UIEnter', {
-  callback = function()
-    vim.o.clipboard = 'unnamed'
-  end,
+vim.api.nvim_create_autocmd("UIEnter", {
+  desc = "Synchronize nvim clipboard with system clipboard",
+  callback = function() vim.o.clipboard = "unnamed" end,
 })
 
-
-
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  callback = function()
-    vim.hl.on_yank()
-  end,
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Highlight when yanking (copying) text",
+  callback = function() vim.hl.on_yank() end,
 })
